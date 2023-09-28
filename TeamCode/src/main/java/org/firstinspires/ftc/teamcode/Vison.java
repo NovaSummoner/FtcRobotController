c75e8528e5a12c8018c54a25f00e3fa9f3fe6fad
329440 <yaitzaaguilar126088@gmail.com>
2023-09-27 17:42:39 -0700

    :)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vison.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+@TeleOp
+public class Vision extends LinearOpMode {
+
+    public class Vison {
+        @Override
+        public void runOpMode() throws InterruptedException {
+
+            AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+                    .setDrawAxes(true)
+                    .setDrawCubeProjection(true)
+                    .setDrawTagID(true)
+                    .setDrawTagOutline(true)
+                    .build();
+
+            VisionPortal visionPortal = new VisionPortal.Builder()
+                    .addProcessor(tagProcessor)
+                    .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                    .setCameraResolution(new Size(648, 488))
+                    .build();
+
+            waitForStart();
+
+            while (!isStopRequested() && opModeIsActive())
+                if (tagProcessor.getDetections().size() > 0)
+        }
+    }
+}
