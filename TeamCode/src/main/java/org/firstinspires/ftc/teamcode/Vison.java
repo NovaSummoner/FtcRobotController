da8770620d1efc79b0b27a54ade83c5a3b4d997b
sync61010 <sncm7513@gmail.com>
2023-10-03 16:34:23 -0700

    ApriltagfinallyworkingIthink

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vison.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vison.java
@@ -7,12 +7,12 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 @TeleOp
 public class Vision extends LinearOpMode {
 
-    public class Vison {
         @Override
         public void runOpMode() throws InterruptedException {
 
@@ -31,8 +31,21 @@ public class Vision extends LinearOpMode {
 
             waitForStart();
 
-            while (!isStopRequested() && opModeIsActive())
-                if (tagProcessor.getDetections().size() > 0)
+            while (!isStopRequested() && opModeIsActive()) {
+
+                if (tagProcessor.getDetections().size() > 0) {
+                    AprilTagDetection tag = tagProcessor.getDetections().get(0);
+
+                    telemetry.addData("x", tag.ftcPose.x);
+                    telemetry.addData("y", tag.ftcPose.y);
+                    telemetry.addData("z", tag.ftcPose.z);
+                    telemetry.addData("roll", tag.ftcPose.roll);
+                    telemetry.addData("pitch", tag.ftcPose.pitch);
+                    telemetry.addData("yaw", tag.ftcPose.yaw);
+                }
+
+                telemetry.update();
+            }
         }
     }
 }
