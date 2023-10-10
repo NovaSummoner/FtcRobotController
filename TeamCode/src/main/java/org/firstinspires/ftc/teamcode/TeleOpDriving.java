7247ab6d8bf2e8b28649d2357743d3cd2ae832b6
sync61010 <sncm7513@gmail.com>
2023-10-10 16:04:24 -0700

    Fixteleopvarnaming AddedIntakemotormaybeorsomethingIthink

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDriving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDriving.java
@@ -14,6 +14,7 @@ public class TeleOpDriving extends OpMode {
     DcMotor back_left;
     DcMotor front_right;
     DcMotor back_right;
+    DcMotor inTakeMotor;
 
 
     @Override
@@ -23,11 +24,12 @@ public class TeleOpDriving extends OpMode {
         back_left = hardwareMap.dcMotor.get("back_left");
         front_right = hardwareMap.dcMotor.get("front_right");
         back_right = hardwareMap.dcMotor.get("back_right");
+        inTakeMotor = hardwareMap.dcMotor.get("inTakeMotor");
         front_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         front_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         back_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         back_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-
+        inTakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
     }
 
@@ -55,37 +57,44 @@ public class TeleOpDriving extends OpMode {
             back_left.setPower(0);
 
         }
-            //Front back Right
+        //Front back Right
         if (Math.abs(-gamepad1.left_stick_x) > .2) {
-                front_left.setPower(-gamepad1.left_stick_x * -1);
-                back_right.setPower(-gamepad1.right_stick_x * -1);
-                back_left.setPower(-gamepad1.left_stick_x * -1);
-                front_right.setPower(-gamepad1.right_stick_x * -1);
+            front_left.setPower(-gamepad1.left_stick_x * -1);
+            back_right.setPower(-gamepad1.right_stick_x * -1);
+            back_left.setPower(-gamepad1.left_stick_x * -1);
+            front_right.setPower(-gamepad1.right_stick_x * -1);
 
-            } else {
-                front_right.setPower(0);
-                back_left.setPower(0);
-                back_right.setPower(0);
-                front_left.setPower(0);
+        } else {
+            front_right.setPower(0);
+            back_left.setPower(0);
+            back_right.setPower(0);
+            front_left.setPower(0);
 
-            }
-                //Front back Left
-            if (Math.abs(gamepad1.right_stick_x) > -1) {
+        }
+        //Front back Left
+        if (Math.abs(gamepad1.right_stick_x) > .2) {
             front_left.setPower(-gamepad1.left_stick_x * -1);
             back_right.setPower(gamepad1.right_stick_x * -1);
             back_left.setPower(gamepad1.left_stick_x * -1);
             front_right.setPower(-gamepad1.right_stick_x * -1);
 
-        } else{
-                front_right.setPower(0);
-                back_left.setPower(0);
-                back_left.setPower(0);
-                front_left.setPower(0);
-    }
+        } else {
+            front_right.setPower(0);
+            back_left.setPower(0);
+            back_left.setPower(0);
+            front_left.setPower(0);
+        }
 
+        //Intake Controller >:D
 
+        if (Math.abs(-gamepad2.left_stick_y) > .2) {
+            inTakeMotor.setPower(-gamepad2.left_stick_y * -1);
+            inTakeMotor.setPower(gamepad2.left_stick_y * -1);
+        } else {
+            inTakeMotor.setPower(0);
 
         }
 
     }
+}
 
