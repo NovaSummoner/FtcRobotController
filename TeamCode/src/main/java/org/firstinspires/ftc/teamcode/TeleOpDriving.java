738030e868cb2de3e833d9642f5378a5e173c4f2
sync61010 <sncm7513@gmail.com>
2023-10-10 15:51:32 -0700

    Fixteleopvarnaming

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDriving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDriving.java
@@ -10,23 +10,23 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class TeleOpDriving extends OpMode {
 
 
-    DcMotor frontLeft;
-    DcMotor backLeft;
-    DcMotor frontRight;
-    DcMotor backRight;
+    DcMotor front_left;
+    DcMotor back_left;
+    DcMotor front_right;
+    DcMotor back_right;
 
 
     @Override
     public void init() {
 
-        frontLeft = hardwareMap.dcMotor.get("frontLeft");
-        backLeft = hardwareMap.dcMotor.get("backLeft");
-        frontRight = hardwareMap.dcMotor.get("frontRight");
-        backRight = hardwareMap.dcMotor.get("backRight");
-        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        front_left = hardwareMap.dcMotor.get("front_left");
+        back_left = hardwareMap.dcMotor.get("back_left");
+        front_right = hardwareMap.dcMotor.get("front_right");
+        back_right = hardwareMap.dcMotor.get("back_right");
+        front_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        front_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        back_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        back_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
 
     }
@@ -35,52 +35,52 @@ public class TeleOpDriving extends OpMode {
     public void loop() {
         //Front back Left
         if (Math.abs(-gamepad1.left_stick_y) > .2) {
-            frontLeft.setPower(-gamepad1.left_stick_y * -1);
-            backLeft.setPower(-gamepad1.left_stick_y * -1);
+            front_left.setPower(-gamepad1.left_stick_y * -1);
+            back_left.setPower(-gamepad1.left_stick_y * -1);
         } else {
-            frontLeft.setPower(0);
-            backLeft.setPower(0);
-            frontRight.setPower(0);
-            backRight.setPower(0);
+            front_left.setPower(0);
+            back_left.setPower(0);
+            front_right.setPower(0);
+            back_right.setPower(0);
         }
 
         //Front back Right
         if (Math.abs(-gamepad1.right_stick_y) > .2) {
-            frontRight.setPower(-gamepad1.right_stick_y * -1);
-            backRight.setPower(-gamepad1.right_stick_y * -1);
+            front_right.setPower(-gamepad1.right_stick_y * -1);
+            back_right.setPower(-gamepad1.right_stick_y * -1);
         } else {
-            frontRight.setPower(0);
-            backRight.setPower(0);
-            frontLeft.setPower(0);
-            backLeft.setPower(0);
+            front_right.setPower(0);
+            back_right.setPower(0);
+            front_left.setPower(0);
+            back_left.setPower(0);
 
         }
             //Front back Right
         if (Math.abs(-gamepad1.left_stick_x) > .2) {
-                frontLeft.setPower(-gamepad1.left_stick_x * -1);
-                backRight.setPower(-gamepad1.right_stick_x * -1);
-                backLeft.setPower(-gamepad1.left_stick_x * -1);
-                frontRight.setPower(-gamepad1.right_stick_x * -1);
+                front_left.setPower(-gamepad1.left_stick_x * -1);
+                back_right.setPower(-gamepad1.right_stick_x * -1);
+                back_left.setPower(-gamepad1.left_stick_x * -1);
+                front_right.setPower(-gamepad1.right_stick_x * -1);
 
             } else {
-                frontRight.setPower(0);
-                backLeft.setPower(0);
-                backRight.setPower(0);
-                frontLeft.setPower(0);
+                front_right.setPower(0);
+                back_left.setPower(0);
+                back_right.setPower(0);
+                front_left.setPower(0);
 
             }
                 //Front back Left
             if (Math.abs(gamepad1.right_stick_x) > -1) {
-            frontLeft.setPower(-gamepad1.left_stick_x * -1);
-            backRight.setPower(gamepad1.right_stick_x * -1);
-            backLeft.setPower(gamepad1.left_stick_x * -1);
-            frontRight.setPower(-gamepad1.right_stick_x * -1);
+            front_left.setPower(-gamepad1.left_stick_x * -1);
+            back_right.setPower(gamepad1.right_stick_x * -1);
+            back_left.setPower(gamepad1.left_stick_x * -1);
+            front_right.setPower(-gamepad1.right_stick_x * -1);
 
         } else{
-                frontRight.setPower(0);
-                backLeft.setPower(0);
-                backLeft.setPower(0);
-                frontLeft.setPower(0);
+                front_right.setPower(0);
+                back_left.setPower(0);
+                back_left.setPower(0);
+                front_left.setPower(0);
     }
 
 
