16cdd8b042dced9e5bde74bdddc3255a1ab9af55
Brandon Tran <327016@my.puhsd>
2023-10-11 16:35:10 -0700

    Hello!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFowardDrive.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "AutoFowardDrive")
+public class AutoFowardDrive extends LinearOpMode {
+
+    DcMotor lf;
+    DcMotor lb;
+    DcMotor rf;
+    DcMotor rb;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        {
+            lf=hardwareMap.dcMotor.get("lf");
+            lb=hardwareMap.dcMotor.get("lb");
+            rf=hardwareMap.dcMotor.get("rf");
+            rb=hardwareMap.dcMotor.get("rb");
+            waitForStart();
+            lf.setPower(-0.5);
+            lb.setPower(-0.5);
+            rf.setPower(0.5);
+            rb.setPower(0.5);
+            sleep(10000);
+        }
+    }
+}
