06f3721d67bec0c875aa261081888b33922c5080
327014 <327014@my.puhsd.org>
2023-10-18 17:43:14 -0700

    hi hiii

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyOpenCvAuto.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@Autonomous(name = "EasyOpenCvDetector", group="Auto")
+public class EasyOpenCvAuto extends LinearOpMode {
+    OpenCvCamera webCam;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        EasyOpenCv detector = new EasyOpenCv(telemetry);
+        webCam.setPipeline(detector);
+        webCam.openCameraDeviceAsync(
+            () -> webCam.startStreaming(2304, 1536, OpenCvCameraRotation.UPRIGHT)
+        );
+
+        waitForStart();
+        switch (detector.getLocation()) {
+            case LEFT:
+                // ...
+                break;
+            case RIGHT:
+                // ...
+                break;
+            case NOT_FOUND:
+                // ...
+        }
+        webCam.stopStreaming();
+    }
+}
