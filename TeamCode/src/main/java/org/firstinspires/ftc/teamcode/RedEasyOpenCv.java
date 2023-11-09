ecb77459c246528be8136a8a7102694af3e62567
327014 <327014@my.puhsd.org>
2023-11-08 17:22:03 -0800

    hi hiii

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedEasyOpenCv.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Scalar;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+
+@Autonomous
+public class RedEasyOpenCv extends LinearOpMode {
+    DcMotor lf, lb, rf, rb;
+
+    static final double COUNTS_PER_MOTOR_REV = 537.7;
+    static final double DRIVE_GEAR_REDUCTION = 1;
+    static final double WHEEL_DIAMETER_INCHES = 3.77953;
+
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double DRIVE_SPEED = 0.2;
+    static final double TURN_SPEED = 0.5;
+
+    private OpenCvCamera webcam;
+    private ContourPipeline pipeline;
+
+    private int minRectangleArea = 2000;
+    private double leftBarcodeRangeBoundary = 0.3;
+    private double rightBarcodeRangeBoundary = 0.7;
+    private double lowerRuntime = 0;
+    private double upperRuntime = 0;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    public static Scalar scalarLowerYCrCb = new Scalar(0.0, 170.0, 0.0);
+    public static Scalar scalarUpperYCrCb = new Scalar(255.0, 255.0, 128.0);
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+        pipeline = new ContourPipeline(0.0, 0.0, 0.0, 0.0);
+
+        pipeline.configureScalarLower(scalarLowerYCrCb.val[0],scalarLowerYCrCb.val[1],scalarLowerYCrCb.val[2]);
+        pipeline.configureScalarUpper(scalarUpperYCrCb.val[0],scalarUpperYCrCb.val[1],scalarUpperYCrCb.val[2]);
+
+        lf = hardwareMap.dcMotor.get("frontLeft");
+        rf = hardwareMap.dcMotor.get("frontRight");
+        lb = hardwareMap.dcMotor.get("backLeft");
+        rb = hardwareMap.dcMotor.get("backRight");
+
+        lf.setDirection(DcMotorSimple.Direction.REVERSE);
+        lb.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+
+}
