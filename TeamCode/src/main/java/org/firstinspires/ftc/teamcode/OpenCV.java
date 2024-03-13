ae5aa627b683c38b87e1be077b4d588240f7cd4d
Brandon Tran <327016@my.puhsd>
2024-03-12 17:36:15 -0700

    easyopencv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV.java
@@ -84,6 +84,7 @@ public class OpenCV extends LinearOpMode {
             double maxArea = 0;
             MatOfPoint largestContour = null;
 
+
             for (MatOfPoint contour : contours) {
                 double area = Imgproc.contourArea(contour);
                 if (area > maxArea) {
